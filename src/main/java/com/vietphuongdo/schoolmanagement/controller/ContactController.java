55da3d140ab59732f3b8a4af589bcae5ecda5d26
VietPhuongDo <vietphuongdo511@gmail.com>
2024-04-04 16:27:22 +0700

    Implement CSRF

--- src/main/java/com/vietphuongdo/schoolmanagement/controller/ContactController.java
+++ src/main/java/com/vietphuongdo/schoolmanagement/controller/ContactController.java
@@ -4,18 +4,13 @@ import com.vietphuongdo.schoolmanagement.model.Contact;
 import com.vietphuongdo.schoolmanagement.service.ContactService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.RequestScope;
-import org.springframework.web.context.annotation.SessionScope;
-import org.springframework.web.servlet.ModelAndView;
 
 
 
@@ -38,7 +33,7 @@ public class ContactController {
     @PostMapping(value = "/saveMsg")
     public String sendMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
         if (errors.hasErrors()){
-            log.error("contact failed due to"+errors.toString());
+            log.error("contact failed due to"+ errors);
             return "contact";
         }
         contactService.sendMessageDetail(contact);
--- src/main/java/com/vietphuongdo/schoolmanagement/controller/HomeController.java
+++ src/main/java/com/vietphuongdo/schoolmanagement/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/home","","/"})
+    @RequestMapping(value = {"/home","/"})
     public String getHomePage(Model model){
         model.addAttribute("username","Phuong");
         return "home";
